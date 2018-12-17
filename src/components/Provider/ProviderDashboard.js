67735e0b15fef951e3576cf406d9af34a6311932
dpolipnick <dick@onlinegrowthsystems.com>
2018-12-17 10:46:23 -0600

    added colors to nav, imported material-ui table

--- src/components/Provider/ProviderDashboard.js
+++ src/components/Provider/ProviderDashboard.js
@@ -1,12 +1,15 @@
+// Vendors
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
+// Components
+import CustomizedTable from './CustomizedTable';
 
 class ProviderDashboard extends Component {
 
   render() {
     return (
       <div>
-
+        <CustomizedTable />
       </div>
     );
   }
