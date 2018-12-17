a8fdd249f8146c01ce90811fff5bafb86cbd28c0
Thomas Angelo Pinataro <tapinataro@csbsju.edu>
2018-12-17 11:52:38 -0600

    base clients table setup with dummy data

--- src/components/Broker/ClientTable.js
+++ src/components/Broker/ClientTable.js
@@ -1,12 +1,69 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 700,
+  },
+});
+
+let id = 0;
+function createData(name, calories, fat, carbs) {
+  id += 1;
+  return { id, name, calories, fat, carbs };
+}
+
+const rows = [
+  createData('Prime', 'Data Sent to Provider', <button>Send to Provider</button>, '12/08/2018'),
+  createData('US Bank', 'Ready For Quote', <button>Send to Provider</button>, '12/09/2018'),
+  createData('Traust', 'Awaiting Data', <button>Send to Provider</button>, '12/10/2018'),
+];
 
 class ClientTable extends Component {
 
   render() {
+    const { classes } = this.props;
     return (
       <div>
-        <h1>Clients</h1>
+        <Paper className={classes.root}>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>Employer</TableCell>
+                <TableCell>Status</TableCell>
+                <TableCell>Send to Provider</TableCell>
+                <TableCell date>Date Sent</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map(row => {
+                return (
+                  <TableRow key={row.id}>
+                    <TableCell component="th" scope="row">
+                    {row.name}
+                    </TableCell>
+                    <TableCell numeric>{row.calories}</TableCell>
+                    <TableCell numeric>{row.fat}</TableCell>
+                    <TableCell numeric>{row.carbs}</TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </Paper>
       </div>
     );
   }
@@ -17,4 +74,8 @@ const mapStateToProps = reduxState => {
   return reduxState
 };
 
-export default connect(mapStateToProps)(ClientTable);
+ClientTable.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(connect(mapStateToProps)(ClientTable));
