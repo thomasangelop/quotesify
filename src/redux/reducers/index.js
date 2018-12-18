4405daf141b793b75436466ffd31759c555ccebd
thomasangelop <me@thomasagnelo.codes>
2018-12-18 12:01:01 -0600

    setup deals saga and reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,6 +3,7 @@ import errors from './errorsReducer';
 import loginMode from './loginModeReducer';
 import user from './userReducer';
 import quotesReducer from './quotesReducer';
+import deals from './dealsReducer';
 
 import types from './typesReducer';
 // rootReducer is the primary reducer for our entire project
@@ -16,7 +17,8 @@ const rootReducer = combineReducers({
   loginMode, // will have a value of 'login' or 'registration' to control which screen is shown
   user, // will have an id and username if someone is logged in
   quotesReducer,
-  types
+  types, 
+  deals //gets deals for broker clients table
 });
 
 export default rootReducer;
