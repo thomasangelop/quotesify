fda1f1fb6ceacd9f370212895631c6a5fe2ef6c4
dpolipnick <dick@onlinegrowthsystems.com>
2018-12-21 15:10:34 -0600

    fixed broker dashboard GET request to view their clients

--- src/components/Broker/ClientTable.js
+++ src/components/Broker/ClientTable.js
@@ -30,7 +30,7 @@ class ClientTable extends Component {
   }
 
   getClients = () => {
-    this.props.dispatch( { type: 'FETCH_CLIENTS', payload: this.props.reduxState.user.user_id } );
+    this.props.dispatch( { type: 'FETCH_CLIENTS', payload: this.props.reduxState.user.company_id } );
   }
 
   render() {
