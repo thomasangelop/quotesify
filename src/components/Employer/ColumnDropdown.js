06f196a179ac60743b42afd650abcc2f7df7d4c6
dpolipnick <dick@onlinegrowthsystems.com>
2019-01-04 14:16:16 -0600

    added sql table headers to the values of the dropdown

--- src/components/Employer/ColumnDropdown.js
+++ src/components/Employer/ColumnDropdown.js
@@ -56,19 +56,21 @@ class ColumnDropdown extends Component {
                   input={<FilledInput name="label" id="filled-label-native-simple" />}
                >
                   <option key='default' disabled={true} value="">-- Select Column Label --</option>
-                  <option value={10}>Employee's Unique ID</option>
-                  <option value={30}>Employee's Date of Birth</option>
-                  <option value={10}>Employee's Date of Hire</option>
-                  <option value={10}>Union or Non-Union</option>
-                  <option value={10}>Employee's Role</option>
-                  <option value={10}>Employee's Salary</option>
-                  <option value={30}>Employee's Gender</option>
-                  <option value={30}>Employment Status (retired, active, LOA, etc.)</option>
-                  <option value={40}>Employee's State of Residence</option>
-                  <option value={40}>Company Code</option>
+                  <option value='employer_supplied_unique_id'>Employee's Unique ID</option>
+                  <option value='date_of_birth'>Employee's Date of Birth</option>
+                  <option value='date_of_hire'>Employee's Date of Hire</option>
+                  <option value='union'>Union or Non-Union</option>
+                  <option value='role'>Employee's Role</option>
+                  <option value='salary_per_year'>Employee's Salary</option>
+                  <option value='gender'>Employee's Gender</option>
+                  <option value='status'>Employment Status (retired, active, LOA, etc.)</option>
+                  <option value='state'>Employee's State of Residence</option>
+                  <option value='employer_supplied_company_code'>Company Code</option>
                </Select>
                <FormHelperText>Select the correct label for the information in the column below.</FormHelperText>
             </FormControl>
+
+            <p>{JSON.stringify(this.state)}</p>
 
          </div>
       );
