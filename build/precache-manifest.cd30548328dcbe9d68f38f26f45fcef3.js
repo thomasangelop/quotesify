2dfd28cd6acdf04dc18f78bc90fcb71014110697
thomasangelop <me@thomasagnelo.codes>
2019-01-17 11:05:52 -0600

    second commit after first build for heroku

---
+++ build/precache-manifest.cd30548328dcbe9d68f38f26f45fcef3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "13ea3cbb8ae78918f897",
+    "url": "/static/js/main.13ea3cbb.chunk.js"
+  },
+  {
+    "revision": "97729bc90c5dc0280ea4",
+    "url": "/static/js/1.97729bc9.chunk.js"
+  },
+  {
+    "revision": "13ea3cbb8ae78918f897",
+    "url": "/static/css/main.d618c5a7.chunk.css"
+  },
+  {
+    "revision": "0ccec7fb9d4249374e523aeb04d8ef17",
+    "url": "/index.html"
+  }
+];
