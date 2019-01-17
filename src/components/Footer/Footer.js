5b8cd53ec3d346a91d638276f176619a1a57bfc5
thomasangelop <me@thomasagnelo.codes>
2019-01-17 11:23:05 -0600

    updated footer comments

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,6 +6,8 @@ import './Footer.css'
 // It doesn't dispatch any redux actions or display any part of redux state
 // or even care what the redux state is, so it doesn't need 'connect()'
 
+//copyright logo and name of app
+
 const Footer = () => (
   <footer>
     &copy; Quotesify
